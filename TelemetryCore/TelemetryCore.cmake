6ff30f9d3fa9c6bc042d981fc6ab0848689cc9ec
Consti10 <geierconstantinabc@gmail.com>
2020-04-06 12:02:29 +0200

    Split CMakeLists.txt into two

--- TelemetryCore/TelemetryCore.cmake
+++ TelemetryCore/TelemetryCore.cmake
@@ -5,33 +5,36 @@
 find_library( log-lib
               log )
 
-set(TELEMETRY_PATH ${CMAKE_CURRENT_LIST_DIR}/src/main/cpp/)
-
+# Add IO and Helper from VideoCore - make sure to
+# set the V_CORE_DIR before including this file
 set(IO_PATH ${V_CORE_DIR}/../VideoTelemetryShared/InputOutput)
 set(HELPER_PATH ${V_CORE_DIR}/../VideoTelemetryShared/Helper)
 include_directories(${HELPER_PATH})
 include_directories(${IO_PATH})
+
+set(T_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/src/main/cpp/)
 
 ####################
 #C-Files
 ####################
 add_library( ltm_frsky_mavlink_smartport
         SHARED
-        ${TELEMETRY_PATH}/cFiles/ltm.c
-        ${TELEMETRY_PATH}/cFiles/frsky.c
-        ${TELEMETRY_PATH}/cFiles/mavlink2.c
-        ${TELEMETRY_PATH}/cFiles/smartport.c
+        ${T_SOURCE_DIR}/cFiles/ltm.c
+        ${T_SOURCE_DIR}/cFiles/frsky.c
+        ${T_SOURCE_DIR}/cFiles/mavlink2.c
+        ${T_SOURCE_DIR}/cFiles/smartport.c
         )
 target_link_libraries(ltm_frsky_mavlink_smartport
         ${log-lib}
         android
         log)
 #######################################################
-include_directories( ${TELEMETRY_PATH}/TelemetryReceiver)
-include_directories( ${TELEMETRY_PATH}/WFBTelemetryData)
+include_directories( ${T_SOURCE_DIR}/TelemetryReceiver)
+include_directories( ${T_SOURCE_DIR}/WFBTelemetryData)
 add_library( TelemetryReceiver
         SHARED
-        ${TELEMETRY_PATH}/TelemetryReceiver/TelemetryReceiver.cpp
+        ${T_SOURCE_DIR}/TelemetryReceiver/TelemetryReceiver.cpp
+        # these 2 files are included in VideoCore
         ${IO_PATH}/FileReader.cpp
         ${IO_PATH}/UDPReceiver.cpp
         )
